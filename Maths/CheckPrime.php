bd4ce28de310577d10cbd0fbbe98a8b3762a5935
Karthik_thotla <33180410+karthikthotla@users.noreply.github.com>
2022-02-09 18:13:52 +0530

    name changed

--- Maths/CheckPrime.php
+++ Maths/CheckPrime.php
@@ -7,7 +7,7 @@
  * @param Integer $number An integer input
  * @return boolean whether the number is prime or not
  */
-//karthik
+//karthiki
 function isPrime(int $number)
 {
     if ($number === 2) {
